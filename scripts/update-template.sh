11a9d7422e0ea533b5b59a510796f3bb3a2d2ca8
Jayden Liang <jliang01@fortinet.com>
2021-05-26 16:11:55 -0700

    add set -eu to update-template.sh and remove commented code

--- scripts/update-template.sh
+++ scripts/update-template.sh
@@ -1,35 +1,13 @@
 #!/bin/sh
 
-exec 3>&1 # make stdout available as fd 3 for the result
-exec 1>&2 # redirect all output to stderr for logging
+exec 3>&1
+exec 1>&2
 
-# apk --no-cache add git > /dev/null
-# git --version
+set -eu
+
 node --version
 npm --version
 
-# # clone the repo from base ($1) to head ($2)
-# echo "git clone $1 $2"
-# git clone $1 $2
-
-# cd $2
-
-# git fetch -fpP
-
-# # check if local branch exists
-# exists=`git show-ref refs/heads/$3`
-# echo "branch $3 ref: $exists"
-# if [ -n "$exists" ]; then
-#     echo "branch exists, replace it with main"
-#     eval "git branch -d $3"
-# else
-#     echo "branch not found"
-# fi
-
-# force drop the current working branch $3 and checkout main to replace it
-# eval "git checkout main && git pull && git branch -d $3 && git checkout -b $3 && git branch -u push-target $3"
-
-# change dir to the git repo directory ($1)
 cd $1
 
 # checkout base branch ($2) into update_branch ($3) for new changes
